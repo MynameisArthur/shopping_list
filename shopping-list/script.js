c88111ebe41ddf22e9e5b305c7521e719184efa2
unknown <arthurd800@wp.pl>
2023-03-22 10:02:04 +0100

    finished project

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -25,6 +25,11 @@ function onAddItemSubmit(e) {
     itemToEdit.classList.remove('edit-mode');
     itemToEdit.remove();
     isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert('That item already exists.');
+      return;
+    }
   }
   addItemToDOM(newItem);
   addItemToStorage(newItem);
@@ -74,6 +79,11 @@ function onClickItem(e) {
   } else {
     setItemToEdit(e.target);
   }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
